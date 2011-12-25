032e2e01fd0f8c6688474a6e0102020c5f021c72
Nat Welch <nat@natwelch.com>
2011-12-24 22:16:50 -0800

    Oh hey, look, I can jump off the screen.

--- lib/natform/player.rb
+++ lib/natform/player.rb
@@ -1,12 +1,27 @@
 class Player < Chingu::GameObject
-  attr_reader :score
+  trait :bounding_box, :scale => 0.8, :debug => false
+  traits :collision_detection, :timer, :velocity
+
   def initialize(options = {})
     super(options.merge(:image => "media/Starfighter.bmp"))
 
     self.input = {
-      :holding_left => :move_left,
-      :holding_right => :move_right
+      [:holding_left, :holding_a] => :move_left,
+      [:holding_right, :holding_d] => :move_right,
+      [:down, :s] => :down,
+      [:up, :w] => :up,
+      [:space] => :jump
     }
+
+    @jumps = 0
+  end
+
+  def up
+    return
+  end
+
+  def down
+    return
   end
 
   def move_left
@@ -15,5 +30,15 @@ class Player < Chingu::GameObject
 
   def move_right
     @x += 1
+  end
+
+  def jump
+    return if @jumps == 1
+    @jumps += 1
+    self.velocity_y = -11
+  end
+
+  def land
+    @jumps = 0
   end
 end
